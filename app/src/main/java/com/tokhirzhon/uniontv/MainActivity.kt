4e5ba3ce9d0c167b474ba8c9790b6e6b8d3c3c28
Takhirzhon <tashmatovtahir@gmail.com>
2023-07-08 14:43:47 +0600

    Initial commit

--- app/src/main/java/com/tokhirzhon/uniontv/MainActivity.kt
+++ app/src/main/java/com/tokhirzhon/uniontv/MainActivity.kt
@@ -1,38 +1,44 @@
 package com.tokhirzhon.uniontv
 
+import android.annotation.SuppressLint
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import android.widget.Button
 import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.database.DatabaseReference
 import com.google.firebase.database.FirebaseDatabase
-import com.google.firebase.ktx.Firebase
+
 
 open class MainActivity : AppCompatActivity() {
 
-    var auth : FirebaseAuth = FirebaseAuth.getInstance()
+    private lateinit var auth: FirebaseAuth
     var db : FirebaseDatabase = FirebaseDatabase.getInstance()
     var users : DatabaseReference = db.getReference("Users")
 
+    @SuppressLint("SuspiciousIndentation")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-
+        //Button войти начало
         val voyti = findViewById<Button>(R.id.login)
         voyti.setOnClickListener {
-            val intent = Intent(this, LogIn::class.java)
+            val intent = Intent(this, Register::class.java)
             startActivity(intent)
-        }
+        }//End of buttom Войти
 
+        //Button нет аккаунта начало
         val reg = findViewById<TextView>(R.id.registerAcc)
         reg.setOnClickListener {
-            val intent = Intent(this, LogIn::class.java)
+            val intent = Intent(this, Register::class.java)
             startActivity(intent)
         }
+        //button нет аккаунта конец
 
 
     }
+
 }
--- app/src/main/java/com/tokhirzhon/uniontv/Register.kt
+++ app/src/main/java/com/tokhirzhon/uniontv/Register.kt
@@ -1,12 +1,61 @@
 package com.tokhirzhon.uniontv
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
 import androidx.appcompat.app.AppCompatActivity
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.FirebaseDatabase
 
 class Register : AppCompatActivity() {
+
+    private lateinit var auth: FirebaseAuth
+    private lateinit var db: FirebaseDatabase
+    private lateinit var users: DatabaseReference
+
+    private lateinit var email: String
+    private lateinit var password: String
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.register_layout)
 
+        auth = FirebaseAuth.getInstance()
+        db = FirebaseDatabase.getInstance()
+        users = db.getReference("Users")
+
+        val emailEditText = findViewById<EditText>(R.id.emailReg)
+        val passwordEditText = findViewById<EditText>(R.id.passwordCreate)
+
+        email = emailEditText.text.toString()
+        password = passwordEditText.text.toString()
+
+        val registerButton = findViewById<Button>(R.id.regbtn)
+        registerButton.setOnClickListener {
+            registerUser()
+        }
+    }
+
+    private fun registerUser() {
+        auth.createUserWithEmailAndPassword(email, password)
+            .addOnCompleteListener { task ->
+                if (task.isSuccessful) {
+                    println("Success")
+                } else {
+                    println("Sorry, something went wrong")
+                }
+            }
+    }
+
+    public override fun onStart() {
+        super.onStart()
+        // Check if user is signed in.
+        if (auth.currentUser == null) {
+            // Not signed in, launch the Sign In activity
+            startActivity(Intent(this, LogIn::class.java))
+            finish()
+        }
     }
 }
